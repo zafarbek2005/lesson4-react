4a03f26b118ec6f39aa0f8f3d3bab6054702d5f3
zafarbek2005 <153996622+zafarbek2005@users.noreply.github.com>
2024-04-14 15:29:21 +0500

    ff

--- src/Components/Main/Main_raboti.jsx
+++ src/Components/Main/Main_raboti.jsx
@@ -3,6 +3,9 @@ import "./Mainraboti.scss"
 import fura from "../Main/img/furaaa.svg"
 import fura2 from "../Main/img/furaaa2.svg"
 import fura3 from "../Main/img/furaaa3.svg"
+import rec from "../Main/img/Rectangle 32.svg"
+import rec2 from "../Main/img/Rectangle 33.svg"
+import rec1 from "../Main/img/Rectangle 34.svg"
 function Main_raboti() {
   return (
     <div className="Conteiner  raboti ">
@@ -30,12 +33,47 @@ function Main_raboti() {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ut.</p>
                 <h5>20 000 руб.</h5>
             </div>
-      
-      
         </div>
+
+        <section className='Conteiner'>
+                <h1 className='etap'>Этапы поставки </h1>
+                <div className="kard">
+                    <div className="ph">
+                        <img src= {rec} alt="" />
+                    </div>
+
+                    <div className="ot">
+                        <h3>Предложение от продавца</h3>
+                        <div className="line"></div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit amet. Ultrices eros in cursus turpis massa tincidunt. Venenatis urna cursus eget nunc scelerisque viverra mauris in. </p>
+                    </div>
+                </div>
+                <div className="kard">
+                    <div className="ph">
+                        <img src= {rec2} alt="" />
+                    </div>
+
+                    <div className="ot">
+                        <h3>Предложение от продавца</h3>
+                        <div className="line"></div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit amet. Ultrices eros in cursus turpis massa tincidunt. Venenatis urna cursus eget nunc scelerisque viverra mauris in. </p>
+                    </div>
+                </div>
+                <div className="kard">
+                    <div className="ph">
+                        <img src= {rec1} alt="" />
+                    </div>
+
+                    <div className="ot">
+                        <h3>Предложение от продавца</h3>
+                        <div className="line"></div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit amet. Ultrices eros in cursus turpis massa tincidunt. Venenatis urna cursus eget nunc scelerisque viverra mauris in. </p>
+                    </div>
+                </div>
+        </section>
     </div>
 
   )
 }
 
-export default Main_raboti
+export default Main_raboti;
